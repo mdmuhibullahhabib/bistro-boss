ff75812ad777648490c095f8995cdc444e1d1097
Arman <mdarman365506@gmail.com>
2024-12-29 17:13:16 +0600

    added testimonials section on home page

--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -2,6 +2,7 @@ import Banner from "../Banner/Banner";
 import Category from "../Category/Category";
 import Featured from "../Featured/Featured";
 import PopularMenu from "../PopularMenu/PopularMenu";
+import Teastimoials from "../Testimonials/Teastimoials";
 
 const Home = () => {
     return (
@@ -9,7 +10,8 @@ const Home = () => {
             <Banner />
             <Category />
             <PopularMenu />
-            <Featured/>
+            <Featured />
+            <Teastimoials/>
         </div>
     );
 };
--- src/pages/Home/PopularMenu/PopularMenu.jsx
+++ src/pages/Home/PopularMenu/PopularMenu.jsx
@@ -17,22 +17,21 @@ const PopularMenu = () => {
     },[])
 
     return (
-        <section className="mb-12">
-            <SectionTitle
-                subHeading='Popular Items'
-                heading='From Our Menu'
-            ></SectionTitle>
+      <section className="mb-12">
+        <SectionTitle
+          subHeading="Popular Items"
+          heading="From Our Menu"
+        ></SectionTitle>
 
-            <div className="grid md:grid-cols-2 gap-10">
-                {
-                    menu.map(item => <MenuItem
-                        key={item._id}
-                        item={item}
-                    ></MenuItem>)
-                }
-            </div>
-            
-        </section>
+        <div className="grid md:grid-cols-2 gap-10">
+          {menu.map((item) => (
+            <MenuItem key={item._id} item={item}></MenuItem>
+          ))}
+        </div>
+          <button className="btn btn-outline border-0 border-b-4 mt-4">
+            View Full Menu
+          </button>
+      </section>
     );
 };
 
