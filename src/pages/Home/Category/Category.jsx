f5abfe3b36e8955addf8cbe4ac8a939d791d9aa2
Arman <mdarman365506@gmail.com>
2024-12-29 14:59:00 +0600

    added dynamic title section

--- src/pages/Home/Category/Category.jsx
+++ src/pages/Home/Category/Category.jsx
@@ -11,10 +11,17 @@ import slider2 from '../../../assets/home/slide2.jpg';
 import slider3 from '../../../assets/home/slide3.jpg';
 import slider4 from '../../../assets/home/slide4.jpg';
 import slider5 from '../../../assets/home/slide5.jpg';
+import SectionTitle from "../../../components/SectionTitle";
 
 const Category = () => {
     return (
-      <div>
+        <div>
+            <SectionTitle
+                subHeading={`From 11.00am to 10.00pm`}
+                heading={"Order Online"}
+                >
+               
+            </SectionTitle>
         <Swiper
           slidesPerView={4}
           spaceBetween={30}
