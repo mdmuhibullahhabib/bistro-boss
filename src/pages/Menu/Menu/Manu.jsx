8137c1c5a21754d7e893a2606c19c89344f247f4
Arman <mdarman365506@gmail.com>
2024-12-29 21:48:03 +0600

    create custom hook and fetch data

--- src/pages/Menu/Menu/Manu.jsx
+++ src/pages/Menu/Menu/Manu.jsx
@@ -1,12 +1,14 @@
 import { Helmet } from "react-helmet-async";
-
+import Cover from "../../../Shared/Cover/Cover";
+import menuImg from '../../../assets/menu/banner3.jpg';
 const Manu = () => {
     return (
         <div>
             <Helmet>
                 <title>Bistro Boss | Menu</title>
             </Helmet>
-            me nu 
+            <Cover img={menuImg} title={'Our menu'} ></Cover>
+          
         </div>
     );
 };
