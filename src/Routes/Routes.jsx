0caab109f75a9e09c8446b758c7064bfea301c61
Arman <mdarman365506@gmail.com>
2024-12-30 11:00:35 +0600

    added order food page and tabs section

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -2,6 +2,7 @@ import { createBrowserRouter } from "react-router-dom";
 import Main from "../Layout/Main";
 import Home from "../pages/Home/Home/Home";
 import Manu from "../pages/Menu/Menu/Manu";
+import Order from "../Order/Order/Order";
 
 export const router = createBrowserRouter([
   {
@@ -16,6 +17,10 @@ export const router = createBrowserRouter([
             path: 'menu',
             element:<Manu/>,
           },
+          {
+            path: 'order',
+            element: <Order/>,
+          }
     ],
   },
 ]);
