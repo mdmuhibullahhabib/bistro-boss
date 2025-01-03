4f17482d7807860165b104d4f41596af22273fcc
Arman <mdarman365506@gmail.com>
2025-01-03 16:05:55 +0600

    npdate count on navbar using retetch tanstack query

--- src/components/FoodCard.jsx
+++ src/components/FoodCard.jsx
@@ -1,31 +1,29 @@
-import Swal from 'sweetalert2';
-import useAuth from '../hooks/useAuth';
-import { useLocation, useNavigate } from 'react-router-dom';
-import useAxiosSecure from '../hooks/useAxiosSecure';
-
-
+import Swal from "sweetalert2";
+import useAuth from "../hooks/useAuth";
+import { useLocation, useNavigate } from "react-router-dom";
+import useAxiosSecure from "../hooks/useAxiosSecure";
+import useCart from "../hooks/useCart";
 
 const FoodCard = ({ item }) => {
-  const { name, image, price, recipe,_id } = item;
+  const { name, image, price, recipe, _id } = item;
   const { user } = useAuth();
   // console.log(user.email);
   const navigate = useNavigate();
   const location = useLocation();
   const axiosSecure = useAxiosSecure();
+  const [, refetch] = useCart();
 
-  
-  const handleAddToCart = food => {
-
+  const handleAddToCart = () => {
     if (user && user?.email) {
-      // TODO: send cart item to the database
-      console.log(user.email, food);
+      //  send cart item to the database
+      // console.log(user.email, food);
       const cartItem = {
         manuId: _id,
         email: user.email,
         name,
         image,
         price,
-      }
+      };
 
       axiosSecure.post("/carts", cartItem).then((res) => {
         console.log(res.data);
@@ -33,14 +31,14 @@ const FoodCard = ({ item }) => {
           Swal.fire({
             position: "top-end",
             icon: "success",
-            title:`${name} added to your cart`,
+            title: `${name} added to your cart`,
             showConfirmButton: false,
             timer: 1500,
           });
-
+          // regetch cart to update the cart i tems count
+          refetch();
         }
       });
-
     } else {
       Swal.fire({
         title: "You are not logged In",
@@ -53,30 +51,34 @@ const FoodCard = ({ item }) => {
       }).then((result) => {
         if (result.isConfirmed) {
           // send the user to the login page
-          navigate('/login', {state: {from: location}})
+          navigate("/login", { state: { from: location } });
         }
       });
     }
-  }
-    
-    return (
-      <div className="card bg-base-100 w-96 shadow-xl">
-        <figure>
-          <img
-            src={image}
-            alt="Shoes"
-          />
-            </figure>
-            <p className="absolute right-0 mr-4 mt-4 px-4 bg-slate-900 text-white">${ price}</p>
-        <div className="card-body flex flex-col items-center">
-                <h2 className="card-title">{name}</h2>
-                <p>{ recipe}</p>
-          <div className="card-actions justify-end">
-            <button onClick={() => handleAddToCart(item)} className="btn btn-outline border-0 border-b-4 mt-4 bg-slate-100 border-orange-400">Add to Cart</button>
-          </div>
+  };
+
+  return (
+    <div className="card bg-base-100 w-96 shadow-xl">
+      <figure>
+        <img src={image} alt="Shoes" />
+      </figure>
+      <p className="absolute right-0 mr-4 mt-4 px-4 bg-slate-900 text-white">
+        ${price}
+      </p>
+      <div className="card-body flex flex-col items-center">
+        <h2 className="card-title">{name}</h2>
+        <p>{recipe}</p>
+        <div className="card-actions justify-end">
+          <button
+            onClick={handleAddToCart}
+            className="btn btn-outline border-0 border-b-4 mt-4 bg-slate-100 border-orange-400"
+          >
+            Add to Cart
+          </button>
         </div>
       </div>
-    );
+    </div>
+  );
 };
 
 export default FoodCard;
