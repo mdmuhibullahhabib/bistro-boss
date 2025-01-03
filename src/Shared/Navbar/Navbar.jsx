8678e09b7a0e8b94a020c688e7ebfdbebcdd73e1
Arman <mdarman365506@gmail.com>
2025-01-03 15:34:48 +0600

    create useCart custom hook and get data using axiosSecure

--- src/Shared/Navbar/Navbar.jsx
+++ src/Shared/Navbar/Navbar.jsx
@@ -2,10 +2,12 @@ import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
 import { AuthContext } from '../../Provider/AuthProvider';
 import { FaShoppingCart } from 'react-icons/fa';
+import useCart from '../../hooks/useCart';
 
 const Navbar = () => {
 
   const { user, logOut } = useContext(AuthContext);
+  const [cart] = useCart();
 
   const handleLogOut = () => {
     logOut()
@@ -30,7 +32,7 @@ const Navbar = () => {
           <Link to="/">
             <button className="btn">
              <FaShoppingCart className='mr-2'></FaShoppingCart>
-              <div className="badge badge-secondary">+0</div>
+              <div className="badge badge-secondary">{ cart.length}</div>
             </button>
           </Link>
         </li>
--- src/main.jsx
+++ src/main.jsx
@@ -1,21 +1,30 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
 
-import {  RouterProvider } from "react-router-dom";
-import { router } from './Routes/Routes';
-import { HelmetProvider } from 'react-helmet-async';
-import AuthProvider from './Provider/AuthProvider';
+import { RouterProvider } from "react-router-dom";
+import { router } from "./Routes/Routes";
+import { HelmetProvider } from "react-helmet-async";
+import AuthProvider from "./Provider/AuthProvider";
+
+import {
+  QueryClient,
+  QueryClientProvider,
+ 
+} from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <AuthProvider>
- 
-      <HelmetProvider>
-        <div className="max-w-screen-xl mx-auto">
-          <RouterProvider router={router} />
-        </div>
-      </HelmetProvider>
+      <QueryClientProvider client={queryClient}>
+        <HelmetProvider>
+          <div className="max-w-screen-xl mx-auto">
+            <RouterProvider router={router} />
+          </div>
+        </HelmetProvider>
+      </QueryClientProvider>
     </AuthProvider>
   </StrictMode>
 );
