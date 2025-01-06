af7a20e556be90fda6bf390db2d42a059be84b83
Arman <mdarman365506@gmail.com>
2025-01-06 10:43:27 +0600

    create useAdmin custom hook for checking admin role

---
+++ src/hooks/useAdmin.jsx
@@ -0,0 +1,19 @@
+import { useQuery } from "@tanstack/react-query";
+import useAuth from "./useAuth";
+import useAxiosPublic from "./useAxiosPublic";
+
+
+const useAdmin = () => {
+    const { user } = useAuth();
+    const axiosSecure = useAxiosPublic();
+    const { data: isAdmin } = useQuery({
+        queryKey: [user?.email, 'isAdmin'],
+        queryFn: async () => {
+            const res = await axiosSecure.get(`/users/admin/${user.email}`);
+            return res?.data?.admin;
+        }
+    })
+    return [isAdmin];
+};
+
+export default useAdmin;
